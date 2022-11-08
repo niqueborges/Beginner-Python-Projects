884006b93c91dc64d83370a3d42e108b9bbefbec
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-08 11:14:54 -0300

    correção

--- madlibs.py
+++ madlibs.py
@@ -13,5 +13,5 @@ verb2 = input("Verb2: ")
 famous_person = input("Famous person: ")
 
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \ 
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
     I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
