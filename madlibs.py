2a174b574c9d844ec067be2818818cd5d4170217
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2022-11-14 13:55:18 -0300

    update

--- madlibs.py
+++ madlibs.py
@@ -9,10 +9,11 @@
 
 adj = input("Adjective: ")
 verb1 = input("Verb: ")
-verb2 = input("Verb2: ")
+verb2 = input("Verb: ")
 famous_person = input("Famous person: ")
 
 
 madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
 I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
 print(madlib)
