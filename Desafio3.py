f18b75980b91336faa3cd93955ab955df2e80a1d
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-12 14:10:32 -0300

    feat: Desafio3

---
+++ Desafio3.py
@@ -0,0 +1,15 @@
+# Desafio com 'Sets'
+
+'''
+Criar um programa que gera 3 listas de acordo com as informações abaixo:
+
+Lista1= Funcionários que tem carro e trabalham à noite
+Lista2= Funcionários que tem carro e trabalham de dia
+Lista3= Funcionários que não tem carro
+
+'''
+funcionarios = ['Ana', 'João', 'Maria', 'José', 'Carlos', 'Mariana', 'Paulo', 'Pedro', 'Luana', 'Luiz']
+turno_dia = ['Ana', 'João', 'Maria', 'José', 'Carlos']
+turno_noite = ['Mariana', 'Paulo', 'Pedro', 'Luana', 'Luiz']
+tem_carro = ['Ana', 'João', 'Maria', 'José', 'Carlos', 'Mariana', 'Paulo', 'Pedro']
+
