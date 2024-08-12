abbccb5ca76e2dbf701bb87b9fd699a2e618f37d
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-12 14:26:53 -0300

    feat: Desafio4

---
+++ Desafio4.py
@@ -0,0 +1,15 @@
+# Calculo de IMC - Indice de Massa Corporal
+
+'''
+Qual é a sua Altura em cm?:
+Qual é o seu Peso em kg?:
+
+'''
+
+# Menor que 18.5: Abaixo do Peso
+# Entre 18.5 e 24.9: Peso Normal
+# Entre 25 e 29.9: Sobrepeso
+# Entre 30 e 39.9: Obesidade Grau 1
+# Acima de 40: Obesidade Grau 2
+
+    
