15a2e3cafa66fa4f51aac6cb00e0fc4d90e4cacf
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-13 13:48:26 -0300

    feat: adicao de exercicios

---
+++ src/solucao05.py
@@ -0,0 +1,17 @@
+# Perdir ao usuário que digite dois numeros
+num1 = float(input('Digite o primeiro numero: '))
+num2 = float(input('Digite o segundo numero: '))
+
+# Realizar os calculos matematicos
+soma = num1 + num2
+subtracao = num1 - num2
+multiplicacao = num1 * num2
+divisao = num1 / num2
+exponenciacao = num1 ** num2
+
+# Imprimindo os resultados na console
+print(f'Soma: {soma}')
+print(f'Subtração: {subtracao}')
+print(f'Multiplicação: {multiplicacao}')
+print(f'Divisão: {divisao}')
+print(f'Exponenciação: {exponenciacao}')
