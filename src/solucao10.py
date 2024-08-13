573d1e3f67c886a273be8a26d6a3b049d0dceb17
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-13 14:31:05 -0300

    feat: Adicionando soluções 10, 11, 12 e 13

---
+++ src/solucao10.py
@@ -0,0 +1,4 @@
+frutas = ['Maça', 'morango', 'uva']
+
+for fruta in frutas:
+    print('Eu gosto de ' + fruta)
---
+++ src/solucao12.py
@@ -0,0 +1,6 @@
+frutas = ['maça', 'banana', 'manga']
+vegetais = ['cenoura', 'alface', 'brocolis']
+
+for fruta in frutas:
+    for vegetal in vegetais:
+        print(fruta, vegetal)
