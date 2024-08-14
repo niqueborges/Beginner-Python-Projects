77a9c94286a544de5de1120029ab3214476de12b
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-08-14 14:15:42 -0300

    feat: add exercicos

---
+++ src/solucao24.py
@@ -0,0 +1,5 @@
+def quadrado(numero):
+    return numero ** 2
+
+num = int(input('Digite um numero: '))
+print(f'O quadrado de {num} é {quadrado(num)}')
---
+++ src/solucao25.py
@@ -0,0 +1,7 @@
+def soma(num1, num2):
+    return num1 + num2
+
+user_num1 = int(input('Digite o primeiro numero: '))
+user_num2 = int(input('Digite o segundo numero: '))
+
+print(f'A soma dos dois numeros é igual a: {soma(user_num1, user_num2)}')
