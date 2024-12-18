a0192b32722c78774c4f9565a4b9d106082d4cda
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-12-18 11:21:26 -0300

    :sparkles: feat: adiciona extração de dados de uma tabela no script de automatização de relatórios

--- src/Project1/automatização_de_relatórios.py
+++ src/Project1/automatização_de_relatórios.py
@@ -2,9 +2,18 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
-#Extrair dados de um site
+# Extrair dados de um site
 url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
 table = soup.find("table", {"data-test": "historical-prices"})
                         
+# Extrair dados de uma tabela
+data = []
+table_body = table.find("tbody")
+rows =table_body.find_all("tr")
+for row in rows:
+    cols = row.find_all("td")
+    cols = [ele.text.strip()for ele in cols]
+    data.append([ele for ele in cols if ele])
+                    
