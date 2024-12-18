233f8f00043908d23482a7867c6c056d5dac5443
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-12-18 10:29:10 -0300

    :sparkles: feat: adiciona extração de dados históricos da AAPL

--- src/Project1/automatização_de_relatórios.py
+++ src/Project1/automatização_de_relatórios.py
@@ -1,3 +1,10 @@
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
+
+#Extrair dados de um site
+url = 'https://finance.yahoo.com/quote/AAPL/history?p=AAPL'
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+table = soup.find("table", {"data-test": "historical-prices"})
+                        
