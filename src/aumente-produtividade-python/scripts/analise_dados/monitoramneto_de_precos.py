6bc9d13da0100d0e59c0a02822701dce0d1524b4
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2024-12-20 17:53:19 -0300

    :sparkles: feat: adiciona função para envio de alertas por e-mail no monitoramento de preços

--- src/aumente-produtividade-python/scripts/analise_dados/monitoramneto_de_precos.py
+++ src/aumente-produtividade-python/scripts/analise_dados/monitoramneto_de_precos.py
@@ -13,3 +13,16 @@ email_addres = "sender@example.com"
 email_password = "password"
 to_email = "receiver@example.com"
 
+# Função para enviar alerta por e-mail
+
+def send_email(subject, body):
+    smtp = smtplib.SMTP("smtp.gmail.com", 587)
+    smtp.ehlo()
+    smtp.starttls()
+    smtp.login(email_addres, email_password)
+    message = f"Subject: {subject}\n\n{body}"
+    smtp.sendmail(email_addres, to_email, message)
+    smtp.quit()
+    
+
+
