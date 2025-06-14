dd0f329c946f879453be5494958485d0f76bc15d
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-06-14 14:07:06 -0300

    :sparkles: feat: add aulas

--- src/cursopython/aula10/aula10.py
+++ src/cursopython/aula10/aula10.py
@@ -1,27 +1,10 @@
 """
-Condições IF, ELIF e ELSE
+Operadores Lógicos
+and, or not
+in e not in
 """
 
-"""if True:
-    print("Verdadeiro.")
+
     
-    num_1 = 2
-    num_2 = 4
-    
-    print(num_1 + num_2)"""
-    
-if False:
-    print('Verdadeiro.')
-    print('teste teste2')
-elif True:
-    print('Agora é verdadeiro.')
-    nome = input('Qual o seu nome? ')
-    
-    print(f'Seu nome é {nome}.')
-elif False:
-    print('Mais uma verdadeira.')
-    print(22 + 22)
-else:
-    print('A minha expressão não é verdadeira.')
-    print('Olá')
+
     
