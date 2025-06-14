2f89d9ac47f31e2e3800d45c313c45ffee08bb67
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-06-14 11:02:23 -0300

    ✨ feat: add aulas

---
+++ src/cursopython/aula10/aula10.py
@@ -0,0 +1,27 @@
+"""
+Condições IF, ELIF e ELSE
+"""
+
+"""if True:
+    print("Verdadeiro.")
+    
+    num_1 = 2
+    num_2 = 4
+    
+    print(num_1 + num_2)"""
+    
+if False:
+    print('Verdadeiro.')
+    print('teste teste2')
+elif True:
+    print('Agora é verdadeiro.')
+    nome = input('Qual o seu nome? ')
+    
+    print(f'Seu nome é {nome}.')
+elif False:
+    print('Mais uma verdadeira.')
+    print(22 + 22)
+else:
+    print('A minha expressão não é verdadeira.')
+    print('Olá')
+    
