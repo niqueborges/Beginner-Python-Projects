9e87bc9ff0d861eb910a50f380b7ed6f955a588f
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-06-14 17:26:36 -0300

    :sparkles: feat: add aulas

---
+++ src/cursopython/aula11/aula11.py
@@ -0,0 +1,30 @@
+"""
+Operadores Lógicos
+and, or, not
+in e not in
+"""
+
+# (Verdadeiro E Verdadeiro) AND
+# (Verdadeiro OR Falso ou Falso OR Verdadeiro) OR
+#
+
+"""a = 2
+b = 3
+
+if b > a:
+    print('B é maior do que A.')
+else:
+    print('A é maior do que B.')
+
+if not b > a:
+    print('B é maior do que A.')
+else:
+    print('A é maior do que B.')
+
+nome = 'Monique Borges.'
+
+if 'o' in nome:
+    print('Existe.')
+else:
+    print('Não existe.')"""
+
