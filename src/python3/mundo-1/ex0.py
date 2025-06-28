c0bffed4900075cee9b41fe60a27279f1a284928
Monique da Silva Borges <95103547+niqueborges@users.noreply.github.com>
2025-06-28 19:12:13 -0300

    ✨ feat: atualizar sequencias_basicas.py para incluir mensagens de boas-vindas e remover variáveis não utilizadas

--- src/python3/mundo-1/ex0.py
+++
@@ -1,4 +0,0 @@
-nome = 'Guanabara'
-idade = 25
-peso = 74.8
-print(nome, idade, peso)
--- src/python3/mundo-1/sequencias_basicas.py
+++ src/python3/mundo-1/sequencias_basicas.py
@@ -1,5 +1,26 @@
+# Escreva um programa que mostre na tela a mensagem "Olá, Mundo!".
+
+print('Olá, Mundo!')
+
+print('---')
+
+# Faça um programa que leia o nome de uma pessoa e mostre uma mensagem de boas-vindas:
+# Ex: 
+# Qual é o seu nome? João da Silva
+# Olá, João da Silva! Prazer em te conhecer!
+
 nome = input('Qual é o seu nome? ')
-idade = input('Quantos anos você tem? ')
-peso = input('Qual é o seu peso? ')
-print(nome, idade, peso)
+print(f'Olá, {nome}! Prazer em te conhecer!')
+
+print('---')
+
+# Crie um programa que leia o nome e o salário de um funcionário, mostrando no final uma mensagem.
+# Ex:
+# Nome do funcionário: Maria do Carmo
+# Salário: R$ 1850,45
+# O funcionário Maria do Carmo tem um salário de R$ 1850,45 em junho de 2025.
+
+nome_funcionario = input('Nome do funcionário: ')
+salario = float(input('Salário: R$ '))
+print(f'O funcionário {nome_funcionario} tem um salário de R$ {salario} em junho de 2025.')
 
